4221049fdfe601267ddbace8f58bad9b5d8d460f
ibrahim0908780850-code <ibrahim0908780850@gmail.com>
2026-07-02 21:38:40 +0200

    Create scr/routes/health.js

---
+++ scr/routes/health.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    status: "SALIH AI CRM RUNNING 🚀",
+    version: "1.0.0"
+  });
+});
+
+export default router;
